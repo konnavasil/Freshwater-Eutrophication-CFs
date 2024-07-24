66a9edd0ee43e35748e57b7e5ae142c7668a858d
konnavasil <159152174+konnavasil@users.noreply.github.com>
2024-07-24 15:32:42 +0200

    Update dis.py

--- Data preprocessing/dis.py
+++ Data preprocessing/dis.py
@@ -1,1 +1,18 @@
 
+############################ 1. DISCHARGE DATA ################################
+# Load the .nc4 file
+nc_file = '../data/...nc4'
+data = xr.open_dataset(nc_file)
+
+# Iterate over each year
+for year in range(2021, 2100):
+    # Select data for the current year
+    yearly_data = data.sel(time=str(year))
+    
+    # Calculate annual average
+    yearly_avg = yearly_data.mean(dim='time')
+
+    # Save new .nc file
+    new_nc_file = f'Discharge_{RCP}_{GCM}_{year}.nc'
+    yearly_avg.to_netcdf(new_nc_file) #m3/s
+    print(f"Annual river discharge data saved to {new_nc_file}")
